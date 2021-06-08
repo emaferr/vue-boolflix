22e635976ff55d95d876cbb8138977f475a0741e
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-08 12:28:01 +0200

    Stampo a video i dati richiesti su ricerca preimpostata

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
+// Milestone 1:
+// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
+// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
+// Titolo
+// Titolo Originale
+// Lingua
+// Voto
+
+
+
 const app = new Vue({
 
     el: '#app',
@@ -5,6 +15,8 @@ const app = new Vue({
     data: {
 
         url : 'https://api.themoviedb.org/3/search/movie?api_key=b9f1f2833add21394f701a0d15da73aa&query=ritorno al futuro',
+
+        risultato : [],
     },
 
 
@@ -22,6 +34,16 @@ const app = new Vue({
 
         .then(resp => {
 
+            let films = resp.data.results;
+
+            for (let index = 0; index < films.length; index++){
+
+                this.risultato.push(films[index])
+
+            }
+
+            console.log(this.risultato);
+            
            
         })
         
