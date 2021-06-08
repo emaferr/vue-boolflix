b13fa8af71d75b0ee3ca7c83339f59af06d3393d
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-08 18:27:29 +0200

    aggiunti commenti

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,8 @@
 // Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato: 
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
 
 // Milestone 2:
-// Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
+
+// gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API.
 
 const app = new Vue({
 
@@ -33,6 +28,7 @@ const app = new Vue({
 
         this.risultato = [],
 
+        // Chiamata film
         axios
 
         .get(this.urlFilms + this.input)
@@ -44,6 +40,9 @@ const app = new Vue({
 
         }),
 
+
+        // Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, 
+        // stando attenti ad avere alla fine dei valori simili
         axios
 
         .get(this.urlSerie + this.input)
