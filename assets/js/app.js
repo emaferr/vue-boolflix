b832f94b8e18e67f6f1e65b70be6e9b93ffb87b0
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-09 10:57:31 +0200

    modificate immagini bambdiere aggiunto voto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
-// Milestone 1:
-
-// Milestone 2:
+// Milestone 1: OK
+// Milestone 2: OK
 
 const app = new Vue({
 
@@ -16,7 +15,7 @@ const app = new Vue({
 
         risultato : [],
 
-        input :'',
+        input :'', 
 
         error : null
 
@@ -26,36 +25,28 @@ const app = new Vue({
     
        ricerca(){
 
+            this.risultato = [];
+
             const chiamataFilm = axios.get(this.urlFilms + this.apiKey + '&query=' + this.input);
             const chiamataSerie = axios.get(this.urlSerie + this.apiKey + '&query=' + this.input);
 
             axios.all([chiamataFilm, chiamataSerie])
-            .then(axios.spread((...resp) => {
-                const risultatoFilm = resp[0].data.results;
-                const risultatoSerie = resp[1].data.results;
-
-                this.risultato = [...risultatoFilm, ...risultatoSerie];
-
+            .then(resp => {
+                for(let i = 0; i < resp.length; i++) {   
+                    this.risultato = this.risultato.concat(resp[i].data.results);
+                }
                 this.input = ''
-
-            }))
-
-        .catch(error => {
-            console.error(error);
-            this.error = "Ci dispiace!, il servizio non è raggiungibile al momento";
-        });
+            })
+            .catch(error => {
+                console.error(error);
+                this.error = "Ci dispiace!, il servizio non è raggiungibile al momento";
+            });
 
        },
 
-       // gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API.
-       getFlag(flagCode){
-        flagCode === "en" ? flagCode = "gb" 
-        : flagCode === "ja" ? flagCode = "jp" 
-        : flagCode === "hi" ? flagCode = "in" 
-        : flagCode = flagCode;
-        
-        return `https://flagcdn.com/28x21/${flagCode}.png`;
-    }
+        giudizio: function(obj) {
+            return Math.ceil(obj / 2);
+          },
 
     },
 
